e6a62fe5357d4a3ed564778d038a5de0a8c9ab1b
Валерій Зінченко <ijsyouridea@Valerijs-MacBook-Pro.local>
2023-12-23 12:51:44 +0100

    chore: added basic php render example

---
+++ syntax.php
@@ -0,0 +1,37 @@
+<?php
+class syntax_plugin_ijsyouridea extends DokuWiki_Syntax_Plugin {
+    function getType() {
+        return 'container';
+    }
+
+    function getPType() {
+        return 'block';
+    }
+
+    function getSort() {
+        return 158;
+    }
+
+    function connectTo($mode) {
+        $this->Lexer->addEntryPattern('<calendar.*?>(?=.*?</calendar>)', $mode, 'plugin_ijsyouridea');
+    }
+
+    function postConnect() {
+        $this->Lexer->addExitPattern('</calendar>', 'plugin_ijsyouridea');
+    }
+
+    function handle($match, $state, $pos, Doku_Handler $handler) {
+        // Handle parameters if needed
+        return array();
+    }
+
+    function render($mode, Doku_Renderer $renderer, $data) {
+        if ($mode == 'xhtml') {
+            $renderer->doc .= '<div id="calendar"></div>';
+            $renderer->script('./front/script.js');
+            $renderer->script('./front/calendar/calendar.min.js');
+            return true;
+        }
+        return false;
+    }
+}
